a9ce95b8820a1c82ad15e8b190380f112d2c7e8e
Burn Pc <seraphim@escsoft.de>
2026-07-15 12:39:01 +0200

    update Relase

--- escsoft.MarqueeTextBlock/Controls/MarqueeTextBlock.xaml.cs
+++ escsoft.MarqueeTextBlock/Controls/MarqueeTextBlock.xaml.cs
@@ -3,7 +3,6 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Animation;
 using System;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace escsoft.Controls;
 
